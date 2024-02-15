08da8d14bd2d5c7f85481baf7de35a9fe73d144c
Georgios Kachrimanis <90828318+georgiosKachrimanis@users.noreply.github.com>
2024-02-15 13:00:09 +0100

    Update categories.py
    added docstrings

--- routes/categories.py
+++ routes/categories.py
@@ -20,6 +20,20 @@ def create_category(
     db: Session = Depends(get_db),
     token: str = Depends(oauth2.oauth2_schema),
 ):
+    """
+    Creates a new category in the database.
+    
+    This endpoint requires admin authentication. It accepts category data conforming to the CategoryBase schema,
+    creates a new category record in the database, and returns the created category's data including the generated ID.
+    
+    Parameters:
+    - category: categories_schemas.CategoryBase - The category data to create.
+    - db: Session - The database session.
+    - token: str - The OAuth2 token for admin authentication.
+    
+    Returns:
+    - The created category's data, including the ID.
+    """
     if oauth2.admin_authentication(token=token):
         return db_categories.create_category(db, category)
 
@@ -27,12 +41,37 @@ def create_category(
 # Get All Categories
 @router.get("/", response_model=List[categories_schemas.CategoryDisplay])
 def get_all_categories(db: Session = Depends(get_db)):
+    """
+    Retrieves all categories from the database.
+    
+    This endpoint fetches and returns a list of all categories currently stored in the database,
+    with each category's information conforming to the CategoryDisplay schema.
+    
+    Parameters:
+    - db: Session - The database session.
+    
+    Returns:
+    - A list of all categories in the database.
+    """
     return db_categories.get_all_categories(db)
 
 
 # Get Category By Id
 @router.get("/{category_id}", response_model=categories_schemas.CategoryDisplay)
 def get_category_by_id(category_id: int, db: Session = Depends(get_db)):
+    """
+    Retrieves a single category by its ID.
+    
+    Fetches and returns the data of a specific category, identified by its ID.
+    If the category with the given ID does not exist, it raises a 404 HTTPException.
+    
+    Parameters:
+    - category_id: int - The ID of the category to retrieve.
+    - db: Session - The database session.
+    
+    Returns:
+    - The data of the requested category.
+    """
     category = db_categories.get_category(db, category_id)
     if category is None:
         raise HTTPException(status_code=404, detail="Category not found")
@@ -48,6 +87,19 @@ def get_movies_by_category(
     category: int,
     db: Session = Depends(get_db),
 ):
+    """
+    Retrieves movies associated with a specific category.
+    
+    This endpoint fetches and returns a list of movies that are associated with the specified category ID.
+    It validates if the category exists before fetching the movies. If the category or movies are not found, it raises a 404 HTTPException.
+    
+    Parameters:
+    - category: int - The ID of the category whose movies are to be fetched.
+    - db: Session - The database session.
+    
+    Returns:
+    - A list of movies associated with the specified category.
+    """
     category_check = get_category_by_id(
         category_id=category,
         db=db,
@@ -76,6 +128,18 @@ def get_movies_by_category(
     response_model=List[movies_schemas.MovieDisplayBase],
 )
 def get_top10_movies_by_category(movies=Depends(get_movies_by_category)):
+    """
+    Retrieves the top 10 movies within a specific category based on average movie rate.
+    
+    This endpoint sorts the movies within a given category by their average movie rate in descending order
+    and returns the top 10 movies.
+    
+    Dependencies:
+    - movies: Depends(get_movies_by_category) - A list of movies fetched based on the category.
+    
+    Returns:
+    - A list of the top 10 movies in the specified category.
+    """
     sorted_movies = sorted(
         movies,
         key=lambda x: x.average_movie_rate,
@@ -94,6 +158,21 @@ def update_category(
     db: Session = Depends(get_db),
     token: str = Depends(oauth2.oauth2_schema),
 ):
+    """
+    Updates an existing category's information.
+    
+    Requires admin authentication to update the data of an existing category, identified by its ID.
+    If the category does not exist, it raises a 404 HTTPException.
+    
+    Parameters:
+    - category_id: int - The ID of the category to update.
+    - request: categories_schemas.CategoryBase - The new data for the category.
+    - db: Session - The database session.
+    - token: str - The OAuth2 token for admin authentication.
+    
+    Returns:
+    - The updated category's data.
+    """
     if oauth2.admin_authentication(token=token):
         category = db_categories.get_category(db, category_id)
         if category is None:
@@ -109,6 +188,20 @@ def delete_category(
     db: Session = Depends(get_db),
     token: str = Depends(oauth2.oauth2_schema),
 ):
+     """
+    Deletes a category from the database.
+    
+    Requires admin authentication. This endpoint allows for the deletion of a category by its ID.
+    If the category does not exist or is associated with a movie, it raises an HTTPException.
+    
+    Parameters:
+    - category_id: int - The ID of the category to delete.
+    - db: Session - The database session.
+    - token: str - The OAuth2 token for admin authentication.
+    
+    Returns:
+    - A success message indicating the category has been deleted.
+    """
     if oauth2.admin_authentication(token=token):
         category = db_categories.get_category(db, category_id)
         if category is None:
