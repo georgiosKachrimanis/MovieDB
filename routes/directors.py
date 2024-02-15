41897a711f59a8beb5558218c5bfb0d5feda0bbb
Georgios Kachrimanis <90828318+georgiosKachrimanis@users.noreply.github.com>
2024-02-15 13:05:13 +0100

    Update directors.py
    added docstrings

--- routes/directors.py
+++ routes/directors.py
@@ -21,6 +21,20 @@ def create_director(
     db: Session = Depends(get_db),
     token: str = Depends(oauth2.oauth2_schema),
 ):
+    """
+    Creates a new director in the database.
+
+    Requires admin authentication. This endpoint creates a new director with the provided data
+    and returns the newly created director's information.
+
+    Parameters:
+    - request: directors_schemas.DirectorBase - The director data to create.
+    - db: Session - The database session.
+    - token: str - The OAuth2 token for admin authentication.
+
+    Returns:
+    - The information of the newly created director.
+    """
     if oauth2.admin_authentication(token=token):
         return db_directors.create_director(db, request)
 
@@ -28,6 +42,19 @@ def create_director(
 # Get Director By Id
 @router.get("/{director_id}", response_model=directors_schemas.DirectorDisplay)
 def get_director_by_id(director_id: int, db: Session = Depends(get_db)):
+    """
+    Retrieves a director by their unique ID.
+
+    Fetches and returns the details of a director identified by their ID. If the director is not found,
+    a 404 HTTPException is raised.
+
+    Parameters:
+    - director_id: int - The unique ID of the director.
+    - db: Session - The database session.
+
+    Returns:
+    - The director's details if found.
+    """
     director = db_directors.get_director(db, director_id)
     if director:
         return director
@@ -41,6 +68,17 @@ def get_director_by_id(director_id: int, db: Session = Depends(get_db)):
 # Get All Directors
 @router.get("/", response_model=List[directors_schemas.DirectorDisplay])
 def read_all_directors(db: Session = Depends(get_db)):
+    """
+    Retrieves all directors from the database.
+
+    Fetches and returns a list of all directors stored in the database, with their details.
+
+    Parameters:
+    - db: Session - The database session.
+
+    Returns:
+    - A list of directors.
+    """
     return db_directors.get_all_directors(db)
 
 
@@ -52,6 +90,22 @@ def update_director(
     db: Session = Depends(get_db),
     token: str = Depends(oauth2.oauth2_schema),
 ):
+    """
+    Updates the details of an existing director.
+
+    Requires admin authentication. This endpoint updates the details of a director identified by their ID
+    with the provided data. If the director is not found, a 404 HTTPException is raised.
+
+    Parameters:
+    - director_id: int - The unique ID of the director to update.
+    - request: directors_schemas.DirectorBase - The new data for the director.
+    - db: Session - The database session.
+    - token: str - The OAuth2 token for admin authentication.
+
+    Returns:
+    - The updated director's details.
+    """
+    
     if oauth2.admin_authentication(token=token):
         director = db_directors.get_director(db, director_id)
         if director is None:
@@ -70,6 +124,20 @@ def delete_director(
     db: Session = Depends(get_db),
     token: str = Depends(oauth2.oauth2_schema),
 ):
+    """
+    Deletes a director from the database.
+
+    Requires admin authentication. This endpoint deletes a director by their ID. If the director does not exist
+    or is associated with movies, an appropriate HTTPException is raised.
+
+    Parameters:
+    - director_id: int - The unique ID of the director to delete.
+    - db: Session - The database session.
+    - token: str - The OAuth2 token for admin authentication.
+
+    Returns:
+    - A success message upon successful deletion.
+    """
     if oauth2.admin_authentication(token=token):
         director = db_directors.get_director(db, director_id)
         if director is None:
