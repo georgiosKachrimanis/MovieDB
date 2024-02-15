2e72ce616a8c6e13ef0063f8a1416afbbd7164e9
Georgios Kachrimanis <90828318+georgiosKachrimanis@users.noreply.github.com>
2024-02-15 13:20:06 +0100

    Update reviews.py
    added docstrings

--- routes/reviews.py
+++ routes/reviews.py
@@ -20,6 +20,23 @@ def create_review(
     db: Session = Depends(get_db),
     token: str = Depends(oauth2.oauth2_schema),
 ):
+    """
+    Creates a new review for a movie.
+    
+    Requires authentication. This endpoint allows authenticated users to post reviews for movies.
+    It validates the existence of the movie in the database before creating the review.
+    
+    Parameters:
+    - request: reviews_schemas.ReviewBase - The review to be created.
+    - db: Session - Dependency injection of the database session.
+    - token: str - The OAuth2 token for user authentication.
+    
+    Returns:
+    - The created review's details as defined by the ReviewDisplayOne schema.
+    
+    Raises:
+    - HTTPException: If the movie is not found or if there's an issue with user authentication.
+    """
     
     try:
         payload = oauth2.decode_access_token(token=token)
@@ -48,6 +65,20 @@ def get_all_reviews(
     user_id: Optional[int] = None,
     movie_id: Optional[int] = None,
     ):
+    """
+    Retrieves all reviews, optionally filtered by user ID or movie ID.
+    
+    Parameters:
+    - db: Session - Dependency injection of the database session.
+    - user_id: Optional[int] - Optional user ID to filter reviews by user.
+    - movie_id: Optional[int] - Optional movie ID to filter reviews by movie.
+    
+    Returns:
+    - A list of reviews, each conforming to the ReviewDisplayAll schema.
+    
+    Raises:
+    - HTTPException: If no reviews are found.
+    """
     reviews = db_reviews.get_all_reviews(db)
     if reviews is None:
         raise HTTPException(
@@ -89,6 +120,16 @@ def get_review_by_id(
     db: Session = Depends(get_db),
 
 ):
+    """
+    Retrieves a review by its ID.
+    
+    Parameters:
+    - review_id: int - The ID of the review to retrieve.
+    - db: Session - Dependency injection of the database session.
+    
+    Returns:
+    - The review's details as defined by the ReviewDisplayOne schema.
+    """
     return db_reviews.get_review_from_db(review_id=review_id, db=db)
 
 
@@ -100,6 +141,23 @@ def patch_review(
     db: Session = Depends(get_db),
     token: str = Depends(oauth2.oauth2_schema),
 ):
+    """
+    Partially updates a review.
+    
+    Requires authentication. This endpoint allows users to partially update their own reviews or for admins to update any review.
+    
+    Parameters:
+    - review_id: int - The ID of the review to update.
+    - request: reviews_schemas.ReviewUpdate - The partial update data.
+    - db: Session - Dependency injection of the database session.
+    - token: str - The OAuth2 token for user authentication.
+    
+    Returns:
+    - The updated review's details as defined by the ReviewDisplayOne schema.
+    
+    Raises:
+    - HTTPException: If the review is not found, not owned by the user, or the user is not an admin.
+    """
     payload = oauth2.decode_access_token(token=token)
     review = db_reviews.get_review_from_db(review_id=review_id, db=db)
 
@@ -128,6 +186,23 @@ def update_review(
     db: Session = Depends(get_db),
     token: str = Depends(oauth2.oauth2_schema),
 ):
+    """
+    Fully updates a review.
+    
+    Requires authentication. This endpoint allows users to fully update their own reviews or for admins to update any review.
+    
+    Parameters:
+    - review_id: int - The ID of the review to be updated.
+    - request: reviews_schemas.ReviewUpdate - The new review data.
+    - db: Session - Dependency injection of the database session.
+    - token: str - The OAuth2 token for user authentication.
+    
+    Returns:
+    - The fully updated review's details as defined by the ReviewDisplayOne schema.
+    
+    Raises:
+    - HTTPException: If the review is not found, not owned by the user, or the user is not an admin.
+    """
     payload = oauth2.decode_access_token(token=token)
     review = db_reviews.get_review_from_db(review_id=review_id, db=db)
 
@@ -154,6 +229,22 @@ def delete_review(
     db: Session = Depends(get_db),
     token: str = Depends(oauth2.oauth2_schema),
 ):
+    """
+    Deletes a review.
+    
+    Requires authentication. This endpoint allows users to delete their own reviews or for admins to delete any review.
+    
+    Parameters:
+    - review_id: int - The ID of the review to be deleted.
+    - db: Session - Dependency injection of the database session.
+    - token: str - The OAuth2 token for user authentication.
+    
+    Returns:
+    - A success message upon deletion.
+    
+    Raises:
+    - HTTPException: If the review is not found, not owned by the user, or the user is not an admin.
+    """
     user_payload = oauth2.decode_access_token(token=token)
     review = db_reviews.get_review_from_db(review_id=review_id, db=db)
 
